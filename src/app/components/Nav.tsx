3ccd607de5c407023b7b4824b610c1b781d4fb79
linx4200 <liuxinran1008@gmail.com>
2025-08-19 18:26:53 +0800

    feat: 添加 Prettier 并实现导航组件
    添加 Prettier 格式化工具及其 Tailwind 插件，同时实现基础导航组件结构。

--- src/app/components/Nav.tsx
+++ src/app/components/Nav.tsx
@@ -1,5 +1,18 @@
+import Link from 'next/link';
+
 const Nav = () => {
-  return <div>Nav</div>;
+  // todo: active status
+  return (
+    <nav>
+      <ul>
+        {/* todo: 是否有可能就改为自动获取到 page.tsx 文件, 而不用在这里手写 */}
+        <li><Link href="/">Home</Link></li>
+        <li><Link href="/projects">Projects</Link></li>
+        <li><Link href="/blog">Blog</Link></li>
+        <li><Link href="/contact">Contact</Link></li>
+      </ul>
+    </nav>
+  );
 };
 
 export default Nav;
