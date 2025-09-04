5d39fb11ffe99331ad4591f92421076c5dd603d2
linx4200 <liuxinran1008@gmail.com>
2025-09-04 15:42:12 +0800

    feat: 完成 nav 的样式

--- src/app/components/Nav.tsx
+++ src/app/components/Nav.tsx
@@ -1,15 +1,41 @@
+'use client';
+
 import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+
+{/* todo: 是否有可能就改为自动获取到 page.tsx 文件, 而不用在这里手写 */}
+const pages = [
+  {
+    name: 'Home',
+    route: '/'
+  },
+  {
+    name: 'Projects',
+    route: '/projects'
+  },
+  {
+    name: 'Blog',
+    route: '/blog'
+  },
+  {
+    name: 'Contact',
+    route: '/contact'
+  }
+];
 
 const Nav = () => {
-  // todo: active status
+  const pathname = usePathname();
   return (
-    <nav>
-      <ul>
-        {/* todo: 是否有可能就改为自动获取到 page.tsx 文件, 而不用在这里手写 */}
-        <li><Link href="/">Home</Link></li>
-        <li><Link href="/projects">Projects</Link></li>
-        <li><Link href="/blog">Blog</Link></li>
-        <li><Link href="/contact">Contact</Link></li>
+    <nav className='flex justify-between h-15 text-base/15  text-center'>
+      <div className='w-50 text-pink-600 font-bold text-2xl/15'>Xinran Liu</div>
+      <ul className='flex w-2xl'>
+        {
+          pages.map(page => (
+            <li key={page.name} className={`flex-1 ${pathname === page.route && 'text-pink-600'}`}>
+              <Link href={page.route}>{page.name}</Link>
+            </li>
+          ))
+        }
       </ul>
     </nav>
   );
